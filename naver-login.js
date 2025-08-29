8ee469008499d85a2b3f761cd82d3cfb1aefc986
ggpt6.choi <ggpt6.choi@gmail.com>
2025-08-29 23:47:10 +0900

    test

--- naver-login.js
+++ naver-login.js
@@ -186,7 +186,7 @@ async function writeBlog({
 // ==========================
 (async () => {
   const browser = await chromium.launch({
-    headless: process.env.CI ? true : false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const context = await browser.newContext({
